1cb96ccf14a2dd9db893443aeaeb53363cfaeeba
NIHAL LIMBU <48156789+insane-arnold@users.noreply.github.com>
2024-02-10 00:43:58 +0530

    Delete lesson2 directory

--- lesson2/CollaborativeFilter.java
+++
@@ -1,9 +0,0 @@
-package io.datajek.spring.basics.movierecommendersystem.lesson2;
-
-public class CollaborativeFilter implements Filter {
-
-	public String[] getRecommendations(String movie) {
-		
-		return new String[] {};
-	}	
-}
